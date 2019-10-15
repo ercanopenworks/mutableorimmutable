b3f7df300b3e7ae229422f4fd1fd7c17f5670888
Ercan Erdoğan <ercanerdogan@gmail.com>
2019-10-15 13:27:16 +0300

    string vs StringBuilder
    It been showed one of differences of  strring and StringBuilder

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //immutable
+        public static void concat1(String s1)
+        {
+
+            // taking a string which 
+            // is to be Concatenate 
+            String st = "forGeeks";
+
+            // using String.Concat method 
+            // you can also replace it with 
+            // s1 = s1 + "forgeeks"; 
+            s1 = String.Concat(s1, st);
+        }
+
+        // Concatenates to StringBuilder 
+        //mutable
+        public static void concat2(StringBuilder s2)
+        {
+
+            // using Append method 
+            // of StringBuilder class 
+            s2.Append("forGeeks");
+        }
+
+     
+        // Main Method 
+        public static void Main(String[] args)
+        {
+
+            String s1 = "Geeks";
+            concat1(s1); // s1 is not changed 
+            Console.WriteLine("Using String Class: " + s1);
+
+            StringBuilder s2 = new StringBuilder("Geeks");
+            concat2(s2); // s2 is changed 
+            Console.WriteLine("Using StringBuilder Class: " + s2);
+
+
+
+        }
+    }
+}
